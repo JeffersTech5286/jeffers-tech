979a1041876c80ae434d399b3fa6e3d747261138
jeffers <jeffersonlima5286@gmail.com>
2025-06-12 12:31:53 -0300

    Corrigido site map

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: '*',
             allow: '/'
         },
-        sitemap: 'https://jeffers-tech.vercel.app/sitemap.xml',
+        sitemap: 'https://www.jefferstech.com.br/sitemap.xml',
     }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,7 @@ import type {MetadataRoute} from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://jeffers-tech.vercel.app',
+            url: 'https://www.jefferstech.com.br/',
             lastModified: new Date().toISOString(),
             changeFrequency: 'weekly',
             priority: 1,
